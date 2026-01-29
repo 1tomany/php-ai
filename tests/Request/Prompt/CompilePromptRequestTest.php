253959cfd6eab885dd243c1146ad4e9dd0ea933a
Vic Cherubini <vcherubini@gmail.com>
2026-01-29 14:25:52 -0600

    wip

--- tests/Request/Prompt/CompilePromptRequestTest.php
+++ tests/Request/Prompt/CompilePromptRequestTest.php
@@ -3,6 +3,7 @@
 namespace OneToMany\AI\Tests\Request\Prompt;
 
 use OneToMany\AI\Request\Prompt\CompilePromptRequest;
+use OneToMany\AI\Request\Prompt\Content\InputText;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 
@@ -14,5 +15,23 @@ final class CompilePromptRequestTest extends TestCase
     public function testConstructingPromptWithNoContentsDoesNotThrowException(): void
     {
         $this->assertCount(0, new CompilePromptRequest('mock', 'mock', [])->getContents());
+    }
+
+    public function testHasContentsIsFalseWhenTheContentsAreEmpty(): void
+    {
+        $request = new CompilePromptRequest('mock', 'mock', []);
+
+        $this->assertFalse($request->hasContents());
+        $this->assertCount(0, $request->getContents());
+    }
+
+    public function testHasContentsIsTrueWhenTheContentsAreNotEmpty(): void
+    {
+        $request = new CompilePromptRequest('mock', 'mock', [
+            new InputText('When was PHP first released?'),
+        ]);
+
+        $this->assertTrue($request->hasContents());
+        $this->assertCount(1, $request->getContents());
     }
 }
