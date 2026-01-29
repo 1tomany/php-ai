8ae858f782061ae1fed4da4bb1eb4d7a9984667c
Vic Cherubini <vcherubini@gmail.com>
2026-01-29 12:39:47 -0600

    wip

--- examples/gemini/cache_file.php
+++ examples/gemini/cache_file.php
@@ -4,7 +4,15 @@ use OneToMany\AI\Client\Gemini\FileClient;
 use OneToMany\AI\Contract\Exception\ExceptionInterface as AiExceptionInterface;
 use OneToMany\AI\Request\File\CacheFileRequest;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\PropertyInfo\Extractor\ConstructorExtractor;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\UnwrappingDenormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
@@ -34,11 +42,29 @@ $httpClient = HttpClient::create([
     ],
 ]);
 
-$fileClient = new FileClient($httpClient, new ObjectNormalizer());
+$propertyInfoExtractor = new PropertyInfoExtractor([], [
+    new ConstructorExtractor([new PhpDocExtractor()]),
+]);
+
+$serializer = new Serializer(
+    normalizers: [
+        new ArrayDenormalizer(),
+        new BackedEnumNormalizer(),
+        new DateTimeNormalizer(),
+        new UnwrappingDenormalizer(),
+
+        // This normalizer must come last to ensure the UnwrappingDenormalizer can be used
+        new ObjectNormalizer(propertyInfoExtractor: $propertyInfoExtractor),
+    ],
+);
+
+$fileClient = new FileClient($httpClient, $serializer);
 
 try {
-    $cacheFileRequest = new CacheFileRequest('gemini', $path, basename($path), filesize($path));
+    $cachedFile = $fileClient->cache(CacheFileRequest::create('gemini', $path));
 } catch (AiExceptionInterface $e) {
-    printf("[ERROR] %s\n", $e->getMessage());
+    printf("[ERROR] %s\n", $e->getPrevious()->getMessage());
     exit(1);
 }
+
+print_r($cachedFile);
