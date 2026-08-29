c5d7db422e78b1d2404b5968cfbcfffa48bc4966
Vic Cherubini <vcherubini@gmail.com>
2026-08-28 22:34:11 -0500

    wip

--- src/Resource/SearchStore/Enum/FileState.php
+++ src/Resource/SearchStore/Enum/FileState.php
@@ -7,4 +7,28 @@ enum FileState
     case Pending;
     case Active;
     case Failed;
+
+    /**
+     * @phpstan-assert-if-true self::Pending $this
+     */
+    public function isPending(): bool
+    {
+        return self::Pending === $this;
+    }
+
+    /**
+     * @phpstan-assert-if-true self::Active $this
+     */
+    public function isActive(): bool
+    {
+        return self::Active === $this;
+    }
+
+    /**
+     * @phpstan-assert-if-true self::Failed $this
+     */
+    public function isFailed(): bool
+    {
+        return self::Failed === $this;
+    }
 }
